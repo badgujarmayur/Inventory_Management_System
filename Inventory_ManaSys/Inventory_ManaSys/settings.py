e8ba7ec8bdfb69efc69bbc8b4e48d6b8d943b5b2
Mayur Badgujar <112198188+badgujarmayur@users.noreply.github.com>
2025-01-22 21:23:55 +0530

    Update settings.py

--- Inventory_ManaSys/Inventory_ManaSys/settings.py
+++ Inventory_ManaSys/Inventory_ManaSys/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$b8524wt*+134=g_9^#)rpe*@*fx=e5qtevxeurj22s7xu@c91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
